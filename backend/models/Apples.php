bd3770259a9e16c783cbbc0a0733af79f31abec2
Bahodir94 <behko@bk.ru>
2020-09-07 14:54:21 +0300

    создание модели для таблицы Apples

---
+++ backend/models/Apples.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+class Apples extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'apples';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['color'], 'required'],
+            [['date_of_apperance', 'date_of_fall'], 'safe'],
+            [['status', 'status_of_ate'], 'integer'],
+            [['color'], 'string', 'max' => 10],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'color' => 'Color',
+            'date_of_apperance' => 'Date Of Apperance',
+            'date_of_fall' => 'Date Of Fall',
+            'status' => 'Status',
+            'status_of_ate' => 'Status Of Ate',
+        ];
+    }
+}
---
+++ backend/models/ApplesSearch.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace backend\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use backend\models\Apples;
+
+/**
+ * ApplesSearch represents the model behind the search form of `backend\models\Apples`.
+ */
+class ApplesSearch extends Apples
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'status', 'status_of_ate'], 'integer'],
+            [['color', 'date_of_apperance', 'date_of_fall'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Apples::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'date_of_apperance' => $this->date_of_apperance,
+            'date_of_fall' => $this->date_of_fall,
+            'status' => $this->status,
+            'status_of_ate' => $this->status_of_ate,
+        ]);
+
+        $query->andFilterWhere(['like', 'color', $this->color]);
+
+        return $dataProvider;
+    }
+}
